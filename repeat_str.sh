7cc31368a21163a876cebafb2d32a70b7a8bf09d
corejuke <korzhuk.anton@gmail.com>
2018-11-12 21:35:23 +0400

    script

--- repeat_str.sh
+++ repeat_str.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-
 FILENAME=$1
 STRING=$2
 COUNT=$3
 
-echo $FILENAME
-echo $STRING
-echo $COUNT
+if [ ! -f "$FILENAME" ]
+    then
+        touch $FILENAME
+        for (( i=1; i <= $COUNT; i++ ))
+        do
+            echo $STRING >> $FILENAME
+        done
+    else
+        echo "error: file $FILENAME already exist"
+fi
